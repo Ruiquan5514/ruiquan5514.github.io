419c689d054c92e2da3a1ca875c91913eb208f7a
Ruiquan5514 <106254662+Ruiquan5514@users.noreply.github.com>
2025-03-29 19:17:25 +0000

    Deploying to gh-pages from @ Ruiquan5514/ruiquan5514.github.io@9f879862e325f85b9689cc933fc7836d018bce05 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -413,14 +413,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%72%7A%68%35%35%31%34@%70%73%75.%65%64%75", "_blank");
         },
       },{
         id: 'social-rss',
@@ -434,14 +427,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=0eo3JGgAAAAJ&hl=en", "_blank");
         },
       },{
       id: 'light-theme',
